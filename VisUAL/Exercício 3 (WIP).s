f3594e9cf9c580b51e244f8ef78c5a047b7cd749
Eduardo Correia <44172778+Educorreia932@users.noreply.github.com>
2019-01-07 20:27:32 +0000

    Update Exercício 3 (WIP).s

--- VisUAL/Exercício 3 (WIP).s
+++ VisUAL/Exercício 3 (WIP).s
@@ -9,13 +9,17 @@ Aux			DCD		0x7F800000, 0x007FFFFF, 0x7FFFFFFF
 			bl		difexpoentes
 			bl		mantissas
 			bl		alinhar
+			bl		sinal
+			bl		operacao
+			bl		normalizar
+			bl		resultado
 			b		fim
 			
 zero			cmp		R0, #0x00000000
-			moveq		R12, R1
+			moveq	R12, R1
 			beq		fim
 			cmp		R1, #0x00000000
-			moveq		R12, R0
+			moveq	R12, R0
 			beq		fim
 			mov		PC, LR
 			
@@ -27,9 +31,10 @@ expoentes		ldr		R2, =Aux
 			and		R3, R3, R1
 			mov		PC, LR
 			
-difexpoentes		cmp		R2, R3
-			subhs		R4, R2, R3
-			sublo		R4, R3, R2
+difexpoentes	cmp		R2, R3
+			subhs	R4, R2, R3
+			sublo	R4, R3, R2
+			lsr		R4, R4, #23
 			mov		PC, LR
 			
 mantissas		ldr		R5, =Aux
@@ -40,6 +45,30 @@ mantissas		ldr		R5, =Aux
 			and		R6, R6, R1
 			mov		PC, LR
 			
-alinhar			cmp		R0, R1
+alinhar		cmp		R2, R3
+			lsrlo	R5, R5, R4
+			lsrhi	R6, R6, R4
+			mov		PC, LR
+			
+sinal		lsl		R7,	R0, #1
+			lsl		R8,	R1, #1
+			mov		PC, LR
+			
+operacao		cmp		R7, R8
+			beq		adicao
+			bne		subtracao
+			
+adicao		add		R11, R5, R6
+			mov		PC, LR
+			
+subtracao		cmp		R5, R6
+			subhs	R11, R5, R6
+			sublo	R11, R6, R5
+			mov		PC, LR
+			
+normalizar	
+			mov		PC, LR
+			
+resultado		mov		PC, LR
 			
 fim			END
