e4aa50d3d8494d940cda0c1c927788bc5793bc11
Eduardo Correia <44172778+Educorreia932@users.noreply.github.com>
2019-01-07 18:01:23 +0000

    Update Exercício 3 (WIP).s

--- VisUAL/Exercício 3 (WIP).s
+++ VisUAL/Exercício 3 (WIP).s
@@ -1,54 +1,45 @@
-NumA			EQU		0x420c0000
-NumB			EQU		0x42040000
+NumA			EQU		0x40900000
+NumB			EQU		0xc0700000
 Aux			DCD		0x7F800000, 0x007FFFFF, 0x7FFFFFFF
 			
 			ldr		R0, =NumA
 			ldr		R1, =NumB
 			bl		zero
 			bl		expoentes
+			bl		difexpoentes
 			bl		mantissas
 			bl		alinhar
-			bl		soma
-			END
+			b		fim
 			
-zero			cmp		R0, #0
-			beq		resultadoA
-			cmp		R1, #0
-			beq		resultadoB
+zero			cmp		R0, #0x00000000
+			moveq	R12, R1
+			beq		fim
+			cmp		R1, #0x00000000
+			moveq	R12, R0
+			beq		fim
 			mov		PC, LR
-			
-resultadoA	mov		R12, R1
-			END
-			
-resultadoB	mov		R12, R0
-			END
 			
 expoentes		ldr		R2, =Aux
 			ldr		R2, [R2]
-			and		R3, R0, R2
-			sub		R3, R3, #127
-			and		R4, R0, R2
-			sub		R4, R4, #127
-			cmp		R3, R4
-			moveq	R5, #0
-			subhi	R5, R3, R4
-			subcc	R5, R4, R3
+			and		R2, R2, R0
+			ldr		R3, =Aux
+			ldr		R3, [R3]
+			and		R3, R3, R1
 			mov		PC, LR
 			
-mantissas		ldr		R2, =Aux
-			ldr		R2, [R2, #4]
-			and		R6, R0, R2
-			and		R7, R1, R2
-			
+difexpoentes	cmp		R2, R3
+			subhs	R4, R2, R3
+			sublo	R4, R3, R2
 			mov		PC, LR
 			
-alinhar		cmp		R3, R4
-			asrhi	R7, R7, R5
-			asrcc	R6, R6, R5
+mantissas		ldr		R5, =Aux
+			ldr		R5, [R5, #4]
+			and		R5, R5, R0
+			ldr		R6, =Aux
+			ldr		R6, [R6, #4]
+			and		R6, R6, R1
 			mov		PC, LR
 			
-soma			add		R8, R6, R7
-			cmp		R3, R4
-			addhs	R8, R8, R3
-			addcc	R8, R8, R3
-			mov		PC, LR
+alinhar		cmp		R0, R1
+			
+fim			END
