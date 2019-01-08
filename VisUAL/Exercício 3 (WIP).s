c917e70b4b3008319019dcebe05dff597d57a3bf
Eduardo Correia <44172778+Educorreia932@users.noreply.github.com>
2019-01-08 18:38:57 +0000

    Update Exercício 3 (WIP).s

--- VisUAL/Exercício 3 (WIP).s
+++ VisUAL/Exercício 3 (WIP).s
@@ -32,7 +32,7 @@ expoentes		ldr		R2, =Aux
 			and		R3, R3, R1
 			mov		PC, LR
 			
-difexpoentes	cmp		R2, R3
+difexpoentes		cmp		R2, R3
 			subhs	R4, R2, R3
 			sublo	R4, R3, R2
 			lsr		R4, R4, #23
@@ -41,23 +41,22 @@ difexpoentes	cmp		R2, R3
 mantissas		ldr		R5, =Aux
 			ldr		R5, [R5, #4]
 			and		R5, R5, R0
-		
 			ldr		R6, =Aux
 			ldr		R6, [R6, #4]
 			and		R6, R6, R1
 			mov		PC, LR
 			
-alinhar		cmp		R2, R3
+alinhar			cmp		R2, R3
 			lsrlo	R5, R5, R4
 			lsrhi	R6, R6, R4
 			mov		PC, LR
 			
-expoentefinal	cmp		R2, R3
+expoentefinal		cmp		R2, R3
 			movhs	R4, R2
 			movlo	R4, R3
 			mov		PC, LR
 			
-sinal		ldr		R7, =Aux
+sinal			ldr		R7, =Aux
 			ldr		R7, [R7, #8]
 			and		R7, R7, R0
 			ldr		R8, =Aux
@@ -69,7 +68,7 @@ operacao		cmp		R7, R8
 			beq		adicao
 			bne		subtracao
 			
-adicao		add		R11, R5, R6
+adicao			add		R11, R5, R6
 			mov		PC, LR
 			
 subtracao		cmp		R5, R6
@@ -77,19 +76,23 @@ subtracao		cmp		R5, R6
 			sublo	R11, R6, R5
 			mov		PC, LR
 			
-normalizar	ldr		R10, =Aux
+normalizar		ldr		R10, =Aux
 			ldr		R10, [R10, #4]
 			and		R10, R10, R11
 			cmp		R10, R11
 			beq		normalizado
 			addne	R9, R9, #1
 			lsr		R11, R11, #1
-			add		R4, R4, #1
+			add		R4, R4, #0x00800000
 			b		normalizar
-normalizado	mov		PC, LR
+normalizado		mov		PC, LR
 			
-resultado		add		R12, R11, R4
-			add		R12, R12, R7
+resultado		lsr		R11, R11, #1
+			add		R4, R4, #0x00800000
+			add		R12, R11, R4
+			cmp		R2, R3
+			addhs	R12, R12, R7
+			addlo	R12, R12, R8
 			mov		PC, LR
 			
 fim			END
